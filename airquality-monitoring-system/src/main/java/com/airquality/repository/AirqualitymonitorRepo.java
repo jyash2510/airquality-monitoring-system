a08aefab38cc2ddf270417fd6ac79ca0cad79f0b
jyash2510 <43602667+jyash2510@users.noreply.github.com>
2022-11-28 15:31:38 +0530

    Delete airquality-monitoring-system directory

--- airquality-monitoring-system/src/main/java/com/airquality/repository/AirqualitymonitorRepo.java
+++
@@ -1,9 +0,0 @@
-package com.airquality.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.airquality.Model.Airqualitymonitor;
-
-public interface AirqualitymonitorRepo extends JpaRepository<Airqualitymonitor, Integer> {
-
-}
